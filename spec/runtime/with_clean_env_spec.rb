93da278e5ba422cabfd1b512ca46d6a18ce6183d
raggi <jftucker@gmail.com>
2010-11-12 09:43:35 -0600

    portable env check

--- spec/runtime/with_clean_env_spec.rb
+++ spec/runtime/with_clean_env_spec.rb
@@ -6,7 +6,7 @@ describe "Bundler.with_clean_env" do
     gem_path = ENV['GEM_PATH']
 
     Bundler.with_clean_env do
-      `echo $GEM_PATH`.strip.should_not == gem_path
+      `#{Gem.ruby} -e "puts ENV['GEM_PATH']"`.strip.should_not == gem_path
     end
 
     ENV['GEM_PATH'].should == gem_path
